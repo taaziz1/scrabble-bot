61f8f4b6a259c248cf8dcc69aced1ff1179b8bcd
Talha <talha.aziz@sjsu.edu>
2026-04-30 20:16:26 -0700

    feat: add support for blanks

--- gaddag/create.py
+++ gaddag/create.py
@@ -8,7 +8,7 @@ import time
 #
 # print(f"loaded in {end1 - start1} seconds")
 
-g = GADDAG(['BOOST'])
+g = GADDAG(['BAGUETTE', 'GETTABLE'])
 
 # print([''.join(p.letters) for p in g.root.final_paths()])
 
@@ -27,14 +27,14 @@ g = GADDAG(['BOOST'])
 # end2 = time.perf_counter()
 # print(f"found in {end2 - start2} seconds")
 
-r = {'A': 0, 'B': 1, 'C': 0, 'D': 0, 'E': 0,
+r = {'A': 1, 'B': 1, 'C': 0, 'D': 0, 'E': 2,
           'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0,
-          'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 2,
-          'P': 0, 'Q': 0, 'R': 0, 'S': 1, 'T': 1,
+          'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0,
+          'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 2,
           'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0,
-          'Z': 0, 'size': 5}
-row = [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None]
-anchor = 6
+          'Z': 0, 'blank': 1, 'size': 7}
+row = [None, None, None, None, None, None, None, 'G', None, None, None, None, None, None, None]
+anchor = 8
 words = []
 
 
@@ -52,7 +52,21 @@ def gen(pos, word, rack, arc):
                             words.append(letter + word)
                         elif anchor + pos == 14 or not row[anchor + pos + 1]:
                             words.append(word + letter)
-                    if arc.destination.arc_exists(letter):
+                    if letter == 'blank':
+                        rack['size'] -= 1
+                        rack['blank'] -= 1
+                        for l in arc.final_letters:
+                            if not l == Path.DELIMITER:
+                                if pos <= 0 and (anchor - pos == 0 or not row[anchor - pos - 1]):
+                                    words.append(l.lower() + word)
+                                elif anchor + pos == 14 or not row[anchor + pos + 1]:
+                                    words.append(word + l.lower())
+                        for l, a in arc.destination.outgoing_arcs.items():
+                            if not l == Path.DELIMITER:
+                                go_on(pos, l.lower(), word, rack, a)
+                        rack['blank'] += 1
+                        rack['size'] += 1
+                    elif arc.destination.arc_exists(letter):
                         rack['size'] -= 1
                         rack[letter] -= 1
                         go_on(pos, letter, word, rack, arc.destination.outgoing_arcs[letter])
--- gaddag/test_gaddag.py
+++ gaddag/test_gaddag.py
@@ -1,4 +1,4 @@
-from random import choices
+from random import sample
 from time import perf_counter
 from GADDAG import Path, GADDAG
 from pickle import load
@@ -8,10 +8,11 @@ def powerset(iterable):
     s = list(iterable)
     return chain.from_iterable(combinations(s, x) for x in range(len(s) + 1))
 
-nwl2023 = load(open("../NWL2023.pickle", "rb"))
+
+nwl2023 = load(open("NWL2023.pickle", "rb"))
 
 start1 = perf_counter()
-g = load(open("../gaddagNWL2023.pickle", "rb"))
+g = load(open("gaddagNWL2023.pickle", "rb"))
 end1 = perf_counter()
 
 print(f"loaded in {end1 - start1} seconds")
@@ -35,7 +36,21 @@ def test_1000_variations():
                                 words.add(letter + word)
                             elif anchor + pos == 14 or not row[anchor + pos + 1]:
                                 words.add(word + letter)
-                        if arc.destination.arc_exists(letter):
+                        if letter == 'blank':
+                            rack['size'] -= 1
+                            rack['blank'] -= 1
+                            for l in arc.final_letters:
+                                if not l == Path.DELIMITER:
+                                    if pos <= 0 and (anchor - pos == 0 or not row[anchor - pos - 1]):
+                                        words.add(l.lower() + word)
+                                    elif anchor + pos == 14 or not row[anchor + pos + 1]:
+                                        words.add(word + l.lower())
+                            for l, a in arc.destination.outgoing_arcs.items():
+                                if not l == Path.DELIMITER:
+                                    go_on(pos, l.lower(), word, rack, a)
+                            rack['blank'] += 1
+                            rack['size'] += 1
+                        elif arc.destination.arc_exists(letter):
                             rack['size'] -= 1
                             rack[letter] -= 1
                             go_on(pos, letter, word, rack, arc.destination.outgoing_arcs[letter])
@@ -57,7 +72,18 @@ def test_1000_variations():
             if anchor + pos < 14:
                 gen(pos + 1, new_word, rack, new_arc)
 
-    uppercase_letters = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+    bag = (
+        ["A"] * 9 + ["B"] * 2 + ["C"] * 2 +
+        ["D"] * 4 + ["E"] * 12 + ["F"] * 2 + ["G"] * 3 +
+        ["H"] * 2 + ["I"] * 9 + ["J"] * 1 + ["K"] * 1 +
+        ["L"] * 4 + ["M"] * 2 + ["N"] * 6 + ["O"] * 8 +
+        ["P"] * 2 + ["Q"] * 1 + ["R"] * 6 + ["S"] * 4 +
+        ["T"] * 6 + ["U"] * 4 + ["V"] * 2 + ["W"] * 2 +
+        ["X"] * 1 + ["Y"] * 2 + ["Z"] * 1 + ["blank"] * 2
+    )
+
+    lowercase_letters = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+
     match = True
     rack_size = 7
     total_words = 0
@@ -66,12 +92,16 @@ def test_1000_variations():
               'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0,
               'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0,
               'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0,
-              'Z': 0, 'size': 0}
+              'Z': 0, 'blank': 0, 'size': 0}
 
-    loop_start = perf_counter()
+    total_time_elapsed = 0
+    iterations = -1
+    max_time = 0
+    most_tedious_rack = []
 
     for _ in range(1000):
-        r = choices(uppercase_letters, k=rack_size)
+        iteration_start = perf_counter()
+        r = sample(bag, k=rack_size)
 
         for letter in r:
             counts[letter] += 1
@@ -82,18 +112,36 @@ def test_1000_variations():
         gen(0, "", counts, g.root_arc)
         for letters in powerset(r):
             for p in permutations(letters):
-                candidate = ''.join(p)
-                if candidate in nwl2023:
-                    final.add(candidate)
+                candidates = [""]
+
+                for c in p:
+                    if c == 'blank':
+                        candidates = [cand + letter for cand in candidates for letter in lowercase_letters]
+                    else:
+                        candidates = [cand + c for cand in candidates]
+                for candidate in candidates:
+                    if candidate.upper() in nwl2023:
+                        final.add(candidate)
         total_words += len(words)
         if final != words:
             print(final)
             print(words)
+            print(words - final)
             match = False
             break
+        iteration_end = perf_counter()
+        time_elapsed = iteration_end - iteration_start
+        total_time_elapsed += time_elapsed
+        if max_time < time_elapsed:
+            max_time = time_elapsed
+            most_tedious_rack = [i for i in counts.items() if i[1] > 0]
+        iterations += 1
         for letter in counts:
             counts[letter] = 0
-    loop_end = perf_counter()
-    print(f"generated {total_words} moves in {loop_end - loop_start} seconds")
+    iterations += 1
+
+    print(f"\ngenerated {total_words} moves in {total_time_elapsed} seconds\n"
+          f"longest iteration: {max_time} seconds, time per iteration: {total_time_elapsed / iterations} seconds\n"
+          f"worst rack: {most_tedious_rack}")
 
     assert match
